65a8822d049d2bc6fd30024dabdb8f3a87e93c8d
greegouze <104724644+greegouze@users.noreply.github.com>
2022-10-18 20:45:34 +0200

    Add files via upload

---
+++ composer/public/index.php
@@ -0,0 +1,26 @@
+
+<?php
+require '../vendor/autoload.php';
+
+
+
+use Wild\App\hello;
+
+$hello = new App\hello();
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Composer</title>
+</head>
+<body>
+    <?php echo $hello->talk(); ?>
+</body>
+</html>
+
+
